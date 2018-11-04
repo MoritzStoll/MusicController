3067428ad7bbf3183a87742b0ef4e165b3d7f1c2
sruisinger <sruisinger@cellular.de>
2018-11-04 16:44:39 +0100

    add gamepad implementation

--- src/gamepad/gamepad.js
+++ src/gamepad/gamepad.js
@@ -1,42 +1,98 @@
+class Gamepad {
+  constructor(mapping) {
+    this.mapping = mapping;
+    this.update = null;
+    window.addEventListener("gamepadconnected", () => this.startGamepad());
+    window.addEventListener("gamepaddisconnected", () => this.stopGamepad());
+    this.initGamepad();
+  }
+
+  startGamepad() {
+    console.log("gamepad connected");
+    this.update = requestAnimationFrame(() => this.loop());
+  }
+
+  stopGamepad() {
+    console.log("gamepad disconnected");
+    cancelAnimationFrame(this.update);
+  }
+
+  loop() {
+    var gamepads = navigator.getGamepads
+      ? navigator.getGamepads()
+      : navigator.webkitGetGamepads
+        ? navigator.webkitGetGamepads
+        : [];
+    if (!gamepads) {
+      return;
+    }
+    var gp = gamepads[0];
+    if (gp.buttons.some(button => button.pressed)) {
+      gp.buttons.forEach((button, i) => {
+        if (button.pressed) {
+          let key = this.mapping.find(button => button.gamepadKeyIndex == i);
+          if (key) {
+            this.clickedButton(key);
+          }
+        }
+      });
+    }
+    this.update = requestAnimationFrame(() => this.loop());
+  }
+
+  clickedButton(key) {
+    key.element.setAttribute("style", `fill: ${key.playColor};`);
+    setTimeout(() => {
+      key.element.setAttribute("style", `fill: ${key.defaultColor};`);
+    }, 100);
+
+    if (key.note) {
+      console.log("play note: ", key.note);
+    } else if (key.chord) {
+      console.log("play chord: ", key.chord);
+    } else if (key.synthesizer) {
+      console.log("call synthesizer function: ", key.synthesizer);
+    }
+  }
+
+  initGamepad() {
+    this.mapping.forEach((key, index) => {
+      let element = document.getElementById(key.id);
+      this.addListener(element, index);
+      this.mapping[index].element = element;
+    });
+  }
+
+  addListener(button, index) {
+    button.addEventListener("click", () => {
+      let key = this.mapping[index];
+      switch (key.status) {
+        case 0:
+          this.changeButtonColor(key.element, key.setupColor);
+          openDropdown(key);
+          break;
+        case 1:
+          console.log("run action for: ", key.id);
+          break;
+        case 2:
+          console.log("play action for: ", key.id);
+          break;
+      }
+    });
+  }
+
+  changeButtonColor(el, color) {
+    el.setAttribute("style", `fill: ${color};`);
+  }
+}
+
 var request = new XMLHttpRequest();
 request.open("GET", "/src/gamepad/gamepadMapping.json", true);
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
-    keys = JSON.parse(request.responseText);
-    initGamepad();
+    new Gamepad(JSON.parse(request.responseText));
   } else {
     console.log("reached target server but return error");
   }
 };
-
 request.send();
-
-function initGamepad() {
-  keys.forEach((key, index) => {
-    let element = document.getElementById(key.id);
-    addListener(element, index);
-    keys[index].element = element;
-  });
-}
-
-function addListener(button, index) {
-  button.addEventListener("click", () => {
-    let key = keys[index];
-    switch (key.status) {
-      case 0:
-        changeButtonColor(key.element, key.setupColor);
-        openDropdown(key);
-        break;
-      case 1:
-        console.log("run action for: ", key.id);
-        break;
-      case 2:
-        console.log("play action for: ", key.id);
-        break;
-    }
-  });
-}
-
-function changeButtonColor(el, color) {
-  el.setAttribute("style", `fill: ${color};`);
-}
