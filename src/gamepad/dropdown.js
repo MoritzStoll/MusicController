23cdcf985ef52f4f5aeeed00b554e6348e196973
sruisinger <sruisinger@cellular.de>
2018-11-07 19:31:24 +0100

    add dropdown element

--- src/gamepad/dropdown.js
+++ src/gamepad/dropdown.js
@@ -1,15 +1,35 @@
 let dropwdown = document.getElementById("dropdown");
-let save = document.getElementById("saveSelection");
+let gamepad = document.getElementById("gamepad");
 
-save.addEventListener("click", () => {
-  closeDropdown();
-});
-
-function openDropdown(el, index) {
+function openDropdown() {
   dropwdown.style.display = "unset";
-  console.log("open dropdown for: ", el.id);
+  dropwdown.style.opacity = 0.9;
+  gamepad.style.opacity = 0.4;
+  let items = ["C#", "D", "F", "C-dur"];
+  createList(items);
+}
+
+function createList(items) {
+  items.forEach((item, i) => {
+    let el = document.createElement("li");
+    el.innerHTML = item;
+    el.id = `item_${i}`;
+    el.addEventListener("click", e => {
+      selectItem(e);
+    });
+    dropwdown.appendChild(el);
+  });
+}
+
+function selectItem(e) {
+  console.log("selected", e);
+  closeDropdown();
 }
 
 function closeDropdown() {
-  dropwdown.style.display = "none";
+  dropwdown.style.opacity = 0;
+  gamepad.style.opacity = 0.8;
+  setTimeout(() => {
+    dropwdown.style.display = "none";
+  }, 300);
 }
