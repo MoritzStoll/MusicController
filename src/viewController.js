1039005e6588e9b1a8d4b223c87dba6d4a7b9de4
sruisinger <sruisinger@cellular.de>
2018-11-25 14:56:13 +0100

    add loading screen

--- src/viewController.js
+++ src/viewController.js
@@ -1,32 +1,6 @@
-class ViewController {
-  constructor() {
-    this.loading = document.getElementById("loadingScreen");
-    this.main = document.getElementById("mainScreen");
-    this.button = document.getElementById("startStopButton");
-    this.dropdown = document.getElementById("dropdown");
-    this.state = 0;
-    this.init();
-  }
+let loading = document.getElementById("loading");
+let mainScreen = document.getElementById("mainScreen");
 
-  init() {
-    //for developmemt hide loading screen
-    this.state = 1;
-
-    this.button.addEventListener("click", () => this.startStop());
-    this.main.style.display = "flex";
-    this.loading.style.display = "none";
-  }
-
-  startStop() {
-    if (this.state === 1) {
-      this.dropdown.style.display = "none";
-      this.button.innerHTML = "stop";
-      this.state = 2;
-    } else if (this.state === 2) {
-      this.button.innerHTML = "start";
-      this.state = 1;
-    }
-  }
+function stopLoading() {
+  loading.style.opacity = 0;
 }
-
-new ViewController();
