afa3816c4b74e4c9bd9b2e7ca8def756d8d92144
sruisinger <sruisinger@cellular.de>
2018-11-27 14:21:50 +0100

    fix tremolo

--- src/synthFunctions.js
+++ src/synthFunctions.js
@@ -5,8 +5,7 @@ function changeVolume(value) {
 
 function changeTremolo(value) {
   value = value / 2 + 0.5;
-
-  if (value < 0.5 && value > 0.49) {
+  if (value < 0.52 && value > 0.48) {
     tremolo.wet.value = 0;
   } else {
     tremolo.wet.value = value;
